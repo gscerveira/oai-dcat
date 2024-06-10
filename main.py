3524dbd864426ebe790e374381186aafc244620f
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-06-10 11:25:17 -0300

    Add comments on main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import re
 # Logging config
 logging.basicConfig(level=logging.DEBUG)
 
+# Initialize a FastAPI app to serve the OAI-PMH endpoint
 app = FastAPI()
 
 
-# Fetch data from an endpoint with httpx
+# Fetch data from an endpoint with httpx (used to get the data from the metadata from the datalake)
 def fetch_data(url):
     with httpx.Client() as client:
         response = client.get(url)
@@ -23,8 +24,12 @@ def fetch_data(url):
 @app.post("/oai")
 def oai(request: Request):
     params = dict(request.query_params)
+
+    # Making sure it uses the dcat_ap metadata prefix
     if 'metadataPrefix' not in params:
         params['metadataPrefix'] = 'dcat_ap'
+
+    # handleRequest points the request to the appropriate method in metadata_provider.py
     response = oai_server.oai_server.handleRequest(params)
     logging.debug(f"OAI-PMH Response: {response}")
     # Replace date in datestamp by empty string
@@ -33,3 +38,4 @@ def oai(request: Request):
 
 
 # To run, use uvicorn main:app --reload
+# To use the OAI-PMH listRecords method, send a request to the following URL: http://localhost:8000/oai?verb=ListRecords
