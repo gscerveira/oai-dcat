51d9db61920939405619906d127246447b7555f5
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-06-17 10:23:10 -0300

    Add new function to main.py

--- main.py
+++ main.py
@@ -39,6 +39,23 @@ def oai(request: Request, dataset_id: str):
     response = re.sub(b'<datestamp>.*</datestamp>', b'', response)
     return Response(content=response, media_type="text/xml")
 
+# Define an endpoint for getting all the datasets
+@app.get("/oai")
+@app.post("/oai")
+def oai_all_datasets(request: Request):
+    params = dict(request.query_params)
+
+    # Making sure it uses the dcat_ap metadata prefix
+    if 'metadataPrefix' not in params:
+        params['metadataPrefix'] = 'dcat_ap'
+
+    # handleRequest points the request to the appropriate method in metadata_provider.py
+    response = oai_server.oai_server.handleRequest(params)
+    logging.debug(f"OAI-PMH Response: {response}")
+    # Replace date in datestamp by empty string
+    response = re.sub(b'<datestamp>.*</datestamp>', b'', response)
+    return Response(content=response, media_type="text/xml")
+
 
 # To run, use uvicorn main:app --reload
 # To use the OAI-PMH listRecords method, send a request to the following URL: http://localhost:8000/oai?verb=ListRecords
